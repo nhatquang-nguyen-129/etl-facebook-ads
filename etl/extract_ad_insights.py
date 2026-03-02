1c6c38ae642068b36902c89c4ad2f487880a64fd
nhatquang-nguyen-129 <nhatquang.nguyen.129@gmail.com>
2026-03-02 18:50:04 +0700

    removed time lib

--- etl/extract_ad_insights.py
+++ etl/extract_ad_insights.py
@@ -3,7 +3,6 @@ from pathlib import Path
 ROOT_FOLDER_LOCATION = Path(__file__).resolve().parents[2]
 sys.path.append(str(ROOT_FOLDER_LOCATION))
 
-import time
 import pandas as pd
 
 from facebook_business.api import FacebookAdsApi
