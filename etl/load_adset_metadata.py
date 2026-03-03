3cd71a6a8cd4e09e0b84050ebb95102d6abeceb4
Nhat Quang Nguyen <nhatquang.nguyen.129@gmail.com>
2026-03-03 14:52:47 +0700

    set mode to upsert

--- etl/load_adset_metadata.py
+++ etl/load_adset_metadata.py
@@ -3,7 +3,6 @@ from pathlib import Path
 ROOT_FOLDER_LOCATION = Path(__file__).resolve().parents[2]
 sys.path.append(str(ROOT_FOLDER_LOCATION))
 
-import logging
 import pandas as pd
 
 from plugins.google_bigquery import internalGoogleBigqueryLoader
@@ -15,25 +14,23 @@ def load_adset_metadata(
 ) -> None:
     """
     Load Facebook Ads adset metadata
-    ---------
-    Workflow:
+    ---
+    Principles:
         1. Validate input DataFrame
         2. Validate output direction for Google BigQuery
         3. Set primary key(s) to account_id and adset_id
         4. Use UPSERT mode with temporary table for deduplication
         5. Make internalGoogleBigQueryLoader API call
-    ---------
+    ---
     Returns:
         None
     """      
 
     if df.empty:
-        msg = ("⚠️ [LOADER] Empty Facebook Ads adset metadata Dataframe then loading will be suspended.")
-        print(msg)
-        logging.warning(msg)
+        print("⚠️ [LOADER] Empty Facebook Ads adset metadata Dataframe then loading will be suspended.")
         return
 
-    msg = (
+    print(
         "🔄 [LOADER] Triggering to load "
         f"{len(df)} row(s) of Facebook Ads adset metadata to Google BigQuery table "
         f"{direction}..."
