78da7ef9f11daa16a5a20f74603c3a8d9666cf8d
Nhat Quang Nguyen <nhatquang.nguyen.129@gmail.com>
2026-03-03 15:51:25 +0700

    added track/pillar/group

--- etl/transform_campaign_metadata.py
+++ etl/transform_campaign_metadata.py
@@ -10,28 +10,26 @@ def transform_campaign_metadata(
 ) -> pd.DataFrame:
     """
     Transform Facebook Ads campaign metadata
-    ---------
-    Workflow:
-        1. Validate input
-        2. Validate missing columns
-        3. Assign enriched columns
-    ---------
+    ---
+    Principles:
+        1. Validate input Dataframe
+        2. Validate required schema columns
+        3. Create copy to prevent side effects
+        4. Parse structured naming convention
+        5. Enrich Dataframe
+    ---
     Returns:
         1. DataFrame:
-            Enforced campaigns metadata records
+            Enforced campaign metadata records
     """
 
-    msg = (
+    print(
         "🔄 [TRANSFORM] Transforming "
         f"{len(df)} row(s) of Facebook Ads campaign metadata..."
     )
-    print(msg)
-    logging.info(msg)
 
     if df.empty:
-        msg = "⚠️ [TRANSFORM] Empty campaign metadata then transformation will be suspended."
-        print(msg)
-        logging.warning(msg)
+        print("⚠️ [TRANSFORM] Empty Facebook Ads campaign metadata then transformation will be suspended.")
         return df
 
     required_cols = {
@@ -50,22 +48,18 @@ def transform_campaign_metadata(
     df = df.copy()
     df["platform"] = "Facebook"
     df = df.assign(
-        objective=df["campaign_name"].fillna("").str.split("_").str[0].fillna("unknown"),
-        region=df["campaign_name"].fillna("").str.split("_").str[1].fillna("unknown"),
-        budget_group_1=df["campaign_name"].fillna("").str.split("_").str[2].fillna("unknown"),
-        budget_group_2=df["campaign_name"].fillna("").str.split("_").str[3].fillna("unknown"),      
-        category_level_1=df["campaign_name"].fillna("").str.split("_").str[4].fillna("unknown"),
-        personnel=df["campaign_name"].fillna("").str.split("_").str[5].fillna("unknown"),
-        track_group=df["campaign_name"].fillna("").str.split("_").str[7].fillna("unknown"),
-        pillar_group=df["campaign_name"].fillna("").str.split("_").str[8].fillna("unknown"),
-        content_group=df["campaign_name"].fillna("").str.split("_").str[9].fillna("unknown"),
+        objective=df["campaign_name"].fillna("").str.split("|").str[0].fillna("unknown"),
+        budget_group=df["campaign_name"].fillna("").str.split("|").str[1].fillna("unknown"),        
+        region=df["campaign_name"].fillna("").str.split("|").str[2].fillna("unknown"),
+        category_level_1=df["campaign_name"].fillna("").str.split("|").str[3].fillna("unknown"),
+        track=df["campaign_name"].fillna("").str.split("|").str[6].fillna("unknown"),
+        pillar=df["campaign_name"].fillna("").str.split("|").str[7].fillna("unknown"),
+        group=df["campaign_name"].fillna("").str.split("|").str[8].fillna("unknown"),
     )
 
-    msg = (
+    print(
         "✅ [TRANSFORM] Successfully transformed "
         f"{len(df)} row(s) of Facebook Ads campaign metadata."
     )
-    print(msg)
-    logging.info(msg)
 
     return df
